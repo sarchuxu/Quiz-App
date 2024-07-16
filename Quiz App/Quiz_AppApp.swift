e25b183f9dd2e74c163ea5524baec13e603bbb6c
Sarah Xu <sar.chu.xu@gmail.com>
2024-07-16 10:15:25 -0700

    Initial Commit

---
+++ Quiz App/Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_AppApp.swift
+//  Quiz App
+//
+//  Created by Sarah Xu on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
